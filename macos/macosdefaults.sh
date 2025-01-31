6b949768be029b4503940b8f0ae6e4b123511a5f
André Wittwer <andre.wittwer@gmail.com>
2025-01-31 14:55:23 +0100

    more settings

--- macos/macosdefaults.sh
+++ macos/macosdefaults.sh
@@ -19,6 +19,8 @@ defaults write com.apple.dock mineffect -string "scale"
 defaults write com.apple.dock minimize-to-application -bool true
 defaults write com.apple.dock show-recents -bool false
 defaults write com.apple.dock launchanim -bool false
+defaults write com.apple.dock "autohide-time-modifier" -float "0"
+defaults write com.apple.dock "show-recents" -bool "false"
 
 # Global settings
 defaults write NSGlobalDomain AppleICUForce24HourTime -bool true
